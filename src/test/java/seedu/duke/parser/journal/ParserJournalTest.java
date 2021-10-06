92554085524604e9cfba88f9090a66d43c1416a1
SvethaMahadevan <svets321@gmail.com>
2021-10-06 16:24:37 +0800

    Update ParserJournalTest

--- src/test/java/seedu/duke/parser/journal/ParserJournalTest.java
+++ src/test/java/seedu/duke/parser/journal/ParserJournalTest.java
@@ -3,9 +3,7 @@ package seedu.duke.parser.journal;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author SvethaMahadevan
- */
+//@author SvethaMahadevan
 class ParserJournalTest {
 
     static ParserJournal parser = new ParserJournal();
