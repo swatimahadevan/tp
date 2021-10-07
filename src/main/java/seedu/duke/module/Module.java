778d11b532c97cdabea0a016da30aff3fd059e95
nvbinh15 <binh.pdc01@gmail.com>
2021-10-07 16:55:40 +0800

    Define Module and ModuleManager classes

---
+++ src/main/java/seedu/duke/module/Module.java
@@ -0,0 +1,120 @@
+package seedu.duke.module;
+
+/**
+ * A representation of a Module.
+ */
+public class Module {
+    private String code;
+    private String name;
+    private double expectedGrade;
+
+    private static final String DEFAULT_MODULE_NAME = "NONE";
+    private static final double DEFAULT_EXPECTED_GRADE = -1;
+
+    /**
+     * Class constructor specifying the module code.
+     *
+     * @param code The module code.
+     */
+    public void Module(String code) {
+        this.code = code;
+        this.name = DEFAULT_MODULE_NAME;
+        this.expectedGrade = DEFAULT_EXPECTED_GRADE;
+    }
+
+    /**
+     * Class constructor specifying the module code and module name.
+     *
+     * @param code The module code.
+     * @param name The module name.
+     */
+    public void Module(String code, String name) {
+        this.code = code;
+        this.name = name;
+        this.expectedGrade = DEFAULT_EXPECTED_GRADE;
+    }
+
+    /**
+     * Class constructor specifying the module code, module name, and the expected grade.
+     *
+     * @param code The module code.
+     * @param name The module name.
+     * @param expectedGrade The expected grade of the module.
+     */
+    public void Module(String code, String name, double expectedGrade) {
+        this.code = code;
+        this.name = name;
+        this.expectedGrade = expectedGrade;
+    }
+
+    /**
+     * Returns the module code.
+     *
+     * @return the module code.
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Returns the module name
+     *
+     * @return the module name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the module expected grade.
+     *
+     * @return the module expected grade.
+     */
+    public double getExpectedGrade() {
+        return expectedGrade;
+    }
+
+    /**
+     * Sets the code of the module.
+     *
+     * @param code The code to be set.
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Sets the name of the module.
+     *
+     * @param name The name to be set.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the expected grade of the module.
+     *
+     * @param expectedGrade The grade to be set.
+     */
+    public void setExpectedGrade(double expectedGrade) {
+        this.expectedGrade = expectedGrade;
+    }
+
+    /**
+     * Overrides toString method of class Object to get the string representation of Module.
+     *
+     * @return The string representation of the module.
+     */
+    @Override
+    public String toString() {
+        if (this.name.equals(DEFAULT_MODULE_NAME) & this.expectedGrade == DEFAULT_EXPECTED_GRADE) {
+            return this.code;
+        } else if (this.expectedGrade == DEFAULT_EXPECTED_GRADE) {
+            return this.code + " | " + this.name;
+        } else {
+            return this.code + " | " + this.name + " | Expected grade: " + this.expectedGrade;
+        }
+    }
+
+}
