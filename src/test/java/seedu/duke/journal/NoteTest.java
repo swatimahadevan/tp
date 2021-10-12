5c60239cb63facdd87476f2bfe14eb535e7db92e
SvethaMahadevan <svets321@gmail.com>
2021-10-13 03:28:40 +0800

    checkstyleTest errors fixing

--- src/test/java/seedu/duke/journal/NoteTest.java
+++ src/test/java/seedu/duke/journal/NoteTest.java
@@ -2,7 +2,7 @@ package seedu.duke.journal;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NoteTest {
     static Note parser = new Note("Trial");
