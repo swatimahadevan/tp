e6b250483b79db77dc7cfae1aaae74740d33438d
swatimahadevan <e0665107@u.nus.edu>
2021-10-13 17:32:55 +0800

    fixed checkstyle

--- src/main/java/seedu/duke/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/duke/commands/DeleteTaskCommand.java
@@ -35,8 +35,7 @@ public class DeleteTaskCommand extends Command {
      * @throws IncorrectNumberOfArgumentsException in case of wrong number of arguments.
      */
     @Override
-    public void execute(Ui ui, Storage storage) throws IOException,
-            IndexNotFoundException, IncorrectNumberOfArgumentsException {
+    public void execute(Ui ui, Storage storage) throws IOException, IncorrectNumberOfArgumentsException, IndexNotFoundException {
         if (this.index > storage.tasksList.getTaskList().size()) {
             throw new IndexNotFoundException();
         }
