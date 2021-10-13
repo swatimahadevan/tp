313f65d1911973d206e12fc4325fd4873171c2cf
swatimahadevan <e0665107@u.nus.edu>
2021-10-13 17:21:48 +0800

    fixed checkstyle

--- src/main/java/seedu/duke/exceptions/InvalidDateException.java
+++ src/main/java/seedu/duke/exceptions/InvalidDateException.java
@@ -2,6 +2,7 @@ package seedu.duke.exceptions;
 
 public class InvalidDateException extends Exception {
     private static String message = "Invalid date given, use format DD-MM-YYYY!";
+
     public InvalidDateException() {
         System.out.println(message);
     }
--- src/main/java/seedu/duke/exceptions/InvalidMonthException.java
+++ src/main/java/seedu/duke/exceptions/InvalidMonthException.java
@@ -2,6 +2,7 @@ package seedu.duke.exceptions;
 
 public class InvalidMonthException extends ClickException {
     private static String message = "The month has to be a value between 01-12!";
+
     public InvalidMonthException() {
     }
 
