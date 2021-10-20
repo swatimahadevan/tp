14f1b3ec07f6712cb9bf71f95016a3028cfb8fdd
swatimahadevan <e0665107@u.nus.edu>
2021-10-20 17:00:04 +0800

    fixed io test

--- src/main/java/seedu/duke/parser/schedule/ParserSchedule.java
+++ src/main/java/seedu/duke/parser/schedule/ParserSchedule.java
@@ -120,7 +120,6 @@ public class ParserSchedule {
             default:
             }
         }
-        System.out.println(input);
         if (isNameArgumentPresent && isDateArgumentPresent) {
             return parseTodoArgumentsArray(input);
         }
