d8258a8f535dd0b78d0001ca6b3c29998f2a7a5e
nvbinh15 <binh.pdc01@gmail.com>
2021-11-02 15:50:51 +0800

    Update IllegalModuleException message

--- src/main/java/seedu/duke/constants/ExceptionMessages.java
+++ src/main/java/seedu/duke/constants/ExceptionMessages.java
@@ -1,5 +1,7 @@
 package seedu.duke.constants;
 
+import static seedu.duke.constants.Messages.NEW_LINE;
+
 public class ExceptionMessages {
 
     public static final String MESSAGE_ILLEGAL_COMMAND = "Illegal command. Type help to get the supported commands.";
@@ -10,8 +12,8 @@ public class ExceptionMessages {
     public static final String MESSAGE_ILLEGAL_CURRENT_CAP = "CAP must be a real number between 0.0 and 5.0";
     public static final String MESSAGE_ILLEGAL_EXPECTED_GRADE = "Illegal expected grade."
             + "The supported grades are A+, A, A-, B+, B, B-, C+, C, D+, D, F, CS, CU, and NA.";
-    public static final String MESSAGE_ILLEGAL_MODULE = "Illegal module."
-            + "Please follow the syntax module c/MODULE_CODE {n/MODULE_NAME mc/MODULAR CREDITS e/EXPECTED_GRADE}";
+    public static final String MESSAGE_ILLEGAL_MODULE = "Illegal module." + NEW_LINE
+            + "Please follow the syntax module add c/MODULE_CODE {n/MODULE_NAME mc/MODULAR CREDITS e/EXPECTED_GRADE}";
     public static final String MESSAGE_ILLEGAL_MODULAR_CREDIT = "The modular credit must be a positive integer.";
 
 }
