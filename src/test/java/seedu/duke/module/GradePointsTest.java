5ebfb6c6191d95f873c0e81846757c29aaed61cb
nvbinh15 <binh.pdc01@gmail.com>
2021-11-02 22:14:05 +0800

    Fix checkstyle error

--- src/test/java/seedu/duke/module/GradePointsTest.java
+++ src/test/java/seedu/duke/module/GradePointsTest.java
@@ -2,7 +2,9 @@ package seedu.duke.module;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GradePointsTest {
 
